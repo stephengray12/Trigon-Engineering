252518884e658183827bbfab57aa094d472f806e
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2024-12-22 15:43:11 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>App</div>
   )
 }
+
+export default App
