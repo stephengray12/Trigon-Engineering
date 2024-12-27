08f27765a3b4150501ff3a0f086a2c2b9544aaa9
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2024-12-27 13:53:07 -0600

    Added Basic Navbar.

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,8 +1,9 @@
 
 const Navbar = () => {
-    return <div>Navbar
+    return <div>
     <div>
-        <ul>
+        <h1>Logo</h1>
+        <ul className="nav-links font-bold text-lg text-gray-800 flex justify-between w-1/2 mx-auto py-4 bg-blue-800 text-white rounded-lg shadow-lg max-w-2xl "> 
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
