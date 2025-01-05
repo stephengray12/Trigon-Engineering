90e424a7a9e2bd4ca11472702152f6fb4789b0fd
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2025-01-05 17:30:15 -0600

    Added API for google map, and style containers.

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,23 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex-grow">
-        <p className="text-2xl font-bold text-white text-center p-4 ">
+      <div className="flex-grow p-8  h-6">
+        <p className="text-2xl font-bold text-white text-center p-2 m-auto">
           &quot; Trigon Engineering, Arkansas Premier distributor of industrial equipment and sales &quot;
         </p>
       </div>
-      <Container/>
+      <div className="flex justify-start items-start p-8">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.377234577377!2d-92.25245572279087!3d34.77127747289468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d2bc693ded17d5%3A0x991920e79e7d2c63!2s1317%20N%20Hills%20Blvd%20%23401%2C%20North%20Little%20Rock%2C%20AR%2072114!5e0!3m2!1sen!2sus!4v1736118474899!5m2!1sen!2sus"
+          width="500"
+          height="350"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <Container />
       <Footer />
     </div>
   );
--- src/assets/components/Container.jsx
+++ src/assets/components/Container.jsx
@@ -1,8 +1,8 @@
 const Container = () => {
   return (
-    <div className="flex flex-wrap items-center justify-center min-h-screen">
-      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80">
-        <h1 className="text-blue-300 font-bold underline">Products</h1>
+    <div className="flex flex-wrap items-center justify-center p-8">
+      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80 hover: hover:bg-blue-200 ">
+        <h1 className="text-white font-bold underline bg-gray-600 text-center">Products</h1>
         <ul>
           <li>Pumps</li>
           <li>Valves</li>
@@ -12,8 +12,8 @@ const Container = () => {
         </ul>
       </div>
 
-      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80">
-        <h1 className="text-blue-300 font-bold underline">Services</h1>
+      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80 hover: hover:bg-blue-200">
+        <h1 className="text-white font-bold underline bg-gray-600 text-center">Services</h1>
         <ul>
           <li>Sales</li>
           <li>Service</li>
@@ -21,11 +21,11 @@ const Container = () => {
           <li>Light fabrication</li>
           <li>Elite customer service</li>
         </ul>
-        <p className="text-blue-300">Our biggest service will always be to provide fast and reliable services to customers with a face you can count on.</p>
+        <p className="text-blue-400 ">Our biggest service will always be to provide fast and reliable services to customers with a face you can count on.</p>
       </div>
 
-      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80">
-        <h1 className="text-blue-300 font-bold underline">Mission</h1>
+      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80 hover: hover:bg-blue-200">
+        <h1 className="text-white font-bold underline bg-gray-600 text-center">Mission</h1>
         <p>
           Since 1980, Trigon has been dedicated to providing high-quality engineered products to the industrial market.
           Specializing in the sale, repair, and maintenance of pumps, and valves. We take pride in delivering reliable,
@@ -35,8 +35,8 @@ const Container = () => {
         </p>
       </div>
 
-      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80">
-        <h1 className="text-blue-300 font-bold underline">Manufacturers</h1>
+      <div className="bg-white p-4 m-2 rounded-lg shadow-lg w-80 h-80 hover: hover:bg-blue-200">
+        <h1 className="text-white font-bold underline bg-gray-600 text-center">Manufacturers</h1>
         <ul>
           <li>AEROVENT</li>
           <li>BRAY VALVE</li>
