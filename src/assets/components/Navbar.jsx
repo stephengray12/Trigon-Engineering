adde8f5a852391cf9f883f024c404d3b49e7fdb3
stephengray12 <sgraysk8er1@gmail.com>
2025-05-19 18:50:51 -0500

    Making color changes per customer request.

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -9,12 +9,13 @@ const Navbar = () => {
        <nav className="sticky top-3 z-50 py-3 backdrop-blur border-b border-neutral-700/80 bg-gray-700 bg-opacity-40 m-4 ">
         <div className="w-full px-4 mx-auto relative text-sm flex justify-between items-center ">
             <div className="flex items-center flex-shrink-0">
-                <h1 className="text-3xl font-bold text-white text-outline-blue text-4xl font-bold">Trigon Engineering</h1>
+              <h1 className="text-5xl font-bold text-stroke-blue">Trigon Engineering</h1>
+
                         
             </div>
             <div className="hidden md:flex space-x-4">
-                <a href="#home" className="text-white text-lg hover:text-green-100 font-bold">Home |</a>
-                <a href="#about" className="text-white text-lg hover:text-green-100 font-bold">About |</a>
+                <a href="#home" className="text-white text-lg hover:text-blue-400 font-bold">Home |</a>
+                <a href="#about" className="text-white text-lg hover:text-blue-400 font-bold">About |</a>
                 <a href="#contact" className="text-white  text-xl hover:text-green-100 bg-blue-500 rounded-full p-1 border-2 border-black hover:shadow-lg hover:shadow-black ">Contact us</a>
             </div>
             <div className="md:hidden">
@@ -30,7 +31,6 @@ const Navbar = () => {
             <div className="md:hidden bg-black opacity-90  items-center justify-center space-y-4 fixed right-0 w-1/2 text-center p-4 mt-4">
                 <a href="#home" className="block text-white hover:text-green-200 py-2">Home</a>
                 <a href="#about" className="block text-white hover:text-green-200 py-2">About</a>
-                <a href="#projects" className="block text-white hover:text-green-200 py-2">Projects</a>
                 <a href="#contact" className="block text-white hover:text-green-200 py-2 ">Contact</a>
             </div>
         )}
