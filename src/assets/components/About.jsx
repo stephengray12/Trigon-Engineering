3aa4e1f04de26a04db5efa80e5cc4c786d0b4240
Stephen Gray <97068845+stephengray12@users.noreply.github.com>
2025-08-10 16:33:29 -0500

    Done for the most part need to link up the contact form and patch over to existing domain and email services.

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
   };
 
   const paragraphs = [
-    `Founded with dedication to delivering precision-engineered solutions, Trigon Engineering is your trusted partner for high-quality wet-process equipment. Headquartered in Little Rock, Arkansas, we serve as a manufacturer’s representative, offering an extensive range of robust industrial pumps, valves, and complementary process equipment tailored to meet the unique demands of your operations.`,
+    `Founded with dedication to delivering precision-engineered solutions, Trigon Engineering is your trusted partner for high-quality wet-process equipment. Headquartered in North Little Rock, Arkansas, we serve as a manufacturer’s representative, offering an extensive range of robust industrial pumps, valves, and complementary process equipment tailored to meet the unique demands of your operations.`,
     `At Trigon Engineering, we understand that reliability, efficiency, and unmatched service are non-negotiable. That’s why we align with leading manufacturers and maintain deep technical expertise to ensure seamless integration of solutions across a broad spectrum of industrial applications.`,
     `Whether you're maintaining municipal systems, optimizing industrial operations, or designing new process infrastructure, Trigon Engineering provides the expertise and the products to make your project a success. With a team of seasoned professionals dedicated to customer service and industrial innovation, we stand ready to support you from concept to installation and beyond.`,
     `Let’s build the future of industrial reliability—together.`,
